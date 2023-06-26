d808dbb9221219b0f85d9f761614d85322ded887
yagnik26 <yagnikgoti26@gmail.com>
2023-06-26 18:20:42 +0530

    ch10.1

---
+++ ch10-1/pal10-1-2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void divisible(){
+	int a;
+	printf("Enter any number :");
+	scanf("%d",&a);
+	if(a%3==0 && a%5==0)
+	{
+		printf("The given number is divisible by both 3&5");
+
+	}
+	else
+	{
+		printf("The given number is divisible by both 3&5");
+
+	}
+}
+void main(){
+	divisible();
+	
+}
---
+++ ch10-1/pal10-1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void cube(){
+	int a,b;
+	printf("Enter any number :");
+	scanf("%d",&a);
+	b=a*a;
+	printf("Cube is  :%d",b);
+
+}
+void main(){
+	cube();
+	
+}
