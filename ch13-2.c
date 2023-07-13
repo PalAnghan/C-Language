b69fd2964a82ce091947f62361a9cb3abf19dc36
PalAnghan <132262746+PalAnghan@users.noreply.github.com>
2023-07-13 18:36:31 +0530

    ch13-2

---
+++ ch13-2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+void main()
+{
+	FILE *fp;
+	int i;
+	
+	fp = fopen("C:\\Users\\123\\Desktop\\new10 by 3&5.txt","w");
+	
+	for(i=1; i<=50; i++)
+	{
+		if(i%15==0)
+		{
+			fprintf(fp,"%d\n",i);
+			
+		}
+	}
+fclose(fp);
+}
+
